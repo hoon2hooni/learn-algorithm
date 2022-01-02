cffacd41307e6163cb10eae8dcc4ea0b5be3b971
hoon2hooni <sanghoon.engineer@gmail.com>
2022-01-02 21:02:13 +0900

    refac: refactor logic from comparing size to check first node

--- stack/stack-linked-list-implementation.js
+++ stack/stack-linked-list-implementation.js
@@ -1,6 +1,3 @@
-/**
- * 강의 듣기전 혼자 만들어본 코드!
- */
 class Node {
   constructor(value) {
     this.value = value;
@@ -16,50 +13,41 @@ class Stack {
   }
 
   push(value) {
-    const pushedNode = new Node(value);
-
-    if (this.size === 0) {
-      this.first = this.last = pushedNode;
+    const newNode = new Node(value);
+    //시작 조건
+    if (!this.first) {
+      this.first = this.last = newNode;
       this.size++;
       return;
     }
 
     //추가되는애의 next를 마지막노드 가리킴
-    pushedNode.next = this.last;
+    newNode.next = this.last;
     //마지막 노드를 추가되는 노드로 갱신시킴
-    this.last = pushedNode;
+    this.last = newNode;
     //사이즈 추가
     this.size++;
     return;
   }
 
   pop() {
-    //경계 1 사이즈 0일때
-    if (this.size === 0) {
+    if (!this.first) {
       throw new Error("this stack is already empty");
     }
-    const poppedValue = this.last.value;
-    //경계 2 사이즈 1일때
-    if (this.size === 1) {
-      this.first = this.last = null;
-      this.size--;
-      return poppedValue;
+    const temp = this.last;
+    if (this.first === this.last) {
+      this.first = null;
     }
-
-    const soonLast = this.last.next;
-    this.last = soonLast;
+    this.last = temp.next;
     this.size--;
-    return poppedValue;
+    return temp.value;
   }
 }
 
-// const aStack = new Stack();
-// aStack.push(3);
-// aStack.push(4);
-// aStack.push(5);
-// console.log(aStack.pop());
-// console.log(aStack.pop());
-// console.log(aStack.pop());
-// aStack.push(1000);
-// aStack.pop();
-// console.log(aStack);
+const aStack = new Stack();
+aStack.push(3);
+aStack.push(4);
+
+console.log(aStack.pop());
+console.log(aStack.pop());
+console.log(aStack);
