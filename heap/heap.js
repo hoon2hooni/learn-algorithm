759125bccd87d8f23a837ea5ab48ccf920283ee4
hoon2hooni <sanghoon.engineer@gmail.com>
2022-01-12 16:28:35 +0900

    imp: solve extract method in Max Heap

--- heap/heap.js
+++ heap/heap.js
@@ -56,6 +56,67 @@ class MaxBinaryHeap {
     this.values[b] = aValue;
     return;
   }
+
+  /**
+   * 1. 루트 제
+   * 2. 맨 마지막 원소 위로 보ㅁ
+   * 3. (반복)
+   *     자식들 보다 크면
+   *        반복문 out
+   *     왼쪽 자식(2n + 1), 오른쪽 자식 비교(2n + 2)
+   *     swap(현재 인덱스, 더큰 자식 인덱스)
+   *     현재 인덱스 = 더큰 자식 인덱스로 초기화
+   *   경계 조건
+   *   1. 자식들 인덱스 있는지?
+   *   2. 자식들 인덱스 하나만 있는지?
+   *
+   * 자식이 없는 경우
+   *
+   * 자식이 1개 있는경우
+   *
+   * 자식이 2개 있는경우
+   */
+  extract() {
+    if (!this.values) {
+      throw new Error("this is already empthy heap");
+    }
+    let idx = 0;
+    const returnValue = this.values[idx];
+    this.values[idx] = this.values.pop();
+    const limitIdx = this.values.length - 1;
+    while (true) {
+      let leftIdx = 2 * idx + 1;
+      let rightIdx = 2 * idx + 2;
+      let parent = this.values[idx];
+      let leftChild = this.values[leftIdx];
+      let rightChild = this.values[rightIdx];
+      //자식이 없는 경우
+      if (leftIdx > limitIdx) {
+        break;
+        //자식이 하나만 있는 경우
+      } else if (rightIdx > limitIdx) {
+        if (leftChild > parent) {
+          this.swap(idx, leftIdx);
+          idx = leftIdx;
+        }
+        break;
+      }
+      //자식 2명 다 있는 경우
+      //부모가 자식들보다 크면
+      if (parent > leftChild && parent > rightChild) {
+        break;
+      }
+
+      if (leftChild > rightChild) {
+        this.swap(idx, leftIdx);
+        idx = leftIdx;
+      } else {
+        this.swap(idx, rightIdx);
+        idx = rightIdx;
+      }
+    }
+    return returnValue;
+  }
 }
 
 export { MaxBinaryHeap };
--- heap/heap.test.js
+++ heap/heap.test.js
@@ -1,12 +1,21 @@
 import { MaxBinaryHeap } from "./heap";
 
-it("test Max Binary Heap", () => {
+describe("check Heap Methods", () => {
   const MBH = new MaxBinaryHeap();
-  const inputArray = [5, 1, 2, 3, 4, 6];
-  for (const value of inputArray) {
-    MBH.insertFromUdemy(value);
-  }
+  it("check insert", () => {
+    const inputArray = [5, 1, 2, 3, 4, 6];
+    for (const value of inputArray) {
+      MBH.insertFromUdemy(value);
+    }
 
-  const answerArray = [6, 4, 5, 1, 3, 2];
-  expect(MBH.values).toEqual(answerArray);
+    const answerArray = [6, 4, 5, 1, 3, 2];
+    expect(MBH.values).toEqual(answerArray);
+  });
+
+  it("check extract", () => {
+    expect(MBH.extract()).toBe(6);
+    expect(MBH.values).toEqual([5, 4, 2, 1, 3]);
+    expect(MBH.extract()).toBe(5);
+    expect(MBH.values).toEqual([4, 3, 2, 1]);
+  });
 });
