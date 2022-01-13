72b762f07db257ebfb61fcc5d2e199baa4fc2577
hoon2hooni <sanghoon.engineer@gmail.com>
2022-01-13 19:43:24 +0900

    imp: improve my hash function

--- hash/hash.js
+++ hash/hash.js
@@ -7,4 +7,14 @@ function hash(key, arrayLen) {
   return key;
 }
 
-export { hash };
+function hashUpgraded(key, arrayLen) {
+  let total = 0;
+  let WEIRD_PRIME = 31;
+  for (let i = 0; i < Math.min(key.length, 100); i++) {
+    let char = key[i];
+    let value = char.charCodeAt(0) - 96;
+    total = (total + WEIRD_PRIME + value) % arrayLen;
+  }
+  return total;
+}
+export { hash, hashUpgraded };
--- hash/hash.test.js
+++ hash/hash.test.js
@@ -1,7 +1,7 @@
-import { hash } from "./hash";
+import { hash as hashUpgraded } from "./hash";
 
 describe("test for good hash", () => {
   it("deterministic", () => {
-    expect(hash("test2", 11)).toBe(hash("test2", 11));
+    expect(hashUpgraded("test2", 11)).toBe(hashUpgraded("test2", 11));
   });
 });
