7e04bb16f44a0477b5e0fe98844b050e5c5e796c
hoon2hooni <sanghoon.engineer@gmail.com>
2022-01-17 11:57:09 +0900

    imp: solve remove Vertex in graph class

--- graph/graph.js
+++ graph/graph.js
@@ -2,6 +2,7 @@ class Graph {
   constructor() {
     this.adjacentList = {};
   }
+  //without solution
   addTwoVertexes(vertexA, vertexB) {
     this.connectTwoVertexes(vertexA, vertexB);
     this.connectTwoVertexes(vertexB, vertexA);
@@ -14,6 +15,39 @@ class Graph {
         this.adjacentList[vertexA].push(vertexB);
       }
     }
+  }
+  //udemy solution
+  addVertex(vertex) {
+    if (!this.adjacentList[vertex]) this.adjacentList.vertex = [];
+  }
+
+  addAnEdge(v1, v2) {
+    this.adjacentList[v1].push(v2);
+    this.adjacentList[v1].push(v1);
+  }
+  removeEdge(v1, v2) {
+    this.adjacentList[v1] = this.adjacentList[v1].filter((v) => v !== v2);
+    this.adjacentList[v2] = this.adjacentList[v2].filter((v) => v !== v1);
+    // let idx2 = this.adjacentList[v1].indexOf(v2);
+    // let idx1 = this.adjacentList[v2].indexOf(v1);
+    // if (idx2 !== undefined) {
+    //   this.adjacentList[v1].splice(idx2, 1);
+    // }
+    // if (idx2 !== undefined) {
+    //   this.adjacentList[v2].splice(idx1, 1);
+    // }
+  }
+
+  removeVertex(v1) {
+    this.adjacentList = Object.keys(this.adjacentList).reduce(
+      (prev, currentV) => {
+        if (currentV !== v1) {
+          prev[currentV] = this.adjacentList[currentV].filter((v) => v !== v1);
+        }
+        return prev;
+      },
+      {}
+    );
   }
 }
 
--- graph/graph.test.js
+++ graph/graph.test.js
@@ -9,5 +9,8 @@ describe("test graph", () => {
     expect(myGraph.adjacentList.a).toEqual(["b"]);
     myGraph.addTwoVertexes("c", "b");
     expect(myGraph.adjacentList.b).toEqual(["a", "c"]);
+    myGraph.removeVertex("a");
+    expect(myGraph.adjacentList.b).toEqual(["c"]);
+    expect(myGraph.adjacentList.hasOwnProperty("a")).toBe(false);
   });
 });
