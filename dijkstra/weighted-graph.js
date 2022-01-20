e31ae72fc16111a60796c65e994419a408c12344
hoon2hooni <sanghoon.engineer@gmail.com>
2022-01-20 10:11:39 +0900

    imp: make weighted graph for dijkstra algorithm

---
+++ dijkstra/weighted-graph.js
@@ -0,0 +1,15 @@
+class WeightedGraph {
+  constructor() {
+    this.adjacentList = {};
+    //{a:[{node:b, weight:90}]}
+    //
+  }
+  addVertex(vertex) {
+    if (!this.adjacentList[vertex]) this.adjacentList[vertex] = [];
+  }
+
+  addEdge(v1, v2, weight) {
+    this.adjacentList[v1].push({ node: v2, weight });
+    this.adjacentList[v1].push({ node: v1, weight });
+  }
+}
