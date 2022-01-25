c1c4df37b7ce79e328e8079ae1a6ac493820486e
hoon2hooni <sanghoon.engineer@gmail.com>
2022-01-25 10:11:33 +0900

    imp: optimize bubble sort check if it have swapped before

--- sorting/bubble-sort.js
+++ sorting/bubble-sort.js
@@ -1,10 +1,17 @@
 function bubbleSort(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = 0; j < arr.length - 1 - i; j++) {
-      if (arr[j] > arr[j + 1]) swap(arr, j, j + 1);
+  let flag = true;
+  for (let i = arr.length; i > 0; i--) {
+    if (!flag) {
+      break;
+    }
+    flag = false;
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+        flag = true;
+      }
     }
   }
-
   return arr;
 }
 
