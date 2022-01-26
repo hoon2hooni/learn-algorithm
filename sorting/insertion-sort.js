fb50e8e7cce09ea2121360cf8a7b7fa8856b1d0e
hoon2hooni <sanghoon.engineer@gmail.com>
2022-01-26 20:29:20 +0900

    imp: solve insertion sort with solution

--- sorting/insertion-sort.js
+++ sorting/insertion-sort.js
@@ -1,17 +1,14 @@
 import { swap } from "./swap";
 function insertionSort(arr) {
+  let currentVal;
   for (let i = 1; i < arr.length; i++) {
     //idx는 계속올라감
-    for (let j = i; j > 0; j--) {
+    currentVal = arr[i];
+    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
       //j는 올라간 인덱스 -1 부터 계속 -1하면서 비교
-      if (arr[j - 1] > arr[j]) {
-        //만약 자기보다 큰거발견하면 스왑
-        swap(arr, j - 1, j);
-      } else {
-        //작은거 발견하면 break
-        break;
-      }
+      arr[j + 1] = arr[j];
     }
+    arr[j + 1] = currentVal;
   }
   return arr;
 }
