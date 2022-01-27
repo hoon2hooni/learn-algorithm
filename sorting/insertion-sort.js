8961bbe2ffa9bc6f9c376f56530db2643cd23b5b
hoon2hooni <sanghoon.engineer@gmail.com>
2022-01-27 21:37:33 +0900

    refac : change insertionSort with es6

--- sorting/insertion-sort.js
+++ sorting/insertion-sort.js
@@ -4,13 +4,21 @@ function insertionSort(arr) {
   for (let i = 1; i < arr.length; i++) {
     //idx는 계속올라감
     currentVal = arr[i];
-    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+    let j = i - 1;
+    for (j; j >= 0 && arr[j] > currentVal; j--) {
+      //[1, 3, 2]
       //j는 올라간 인덱스 -1 부터 계속 -1하면서 비교
       arr[j + 1] = arr[j];
     }
     arr[j + 1] = currentVal;
   }
   return arr;
+
+  //https://stackabuse.com/insertion-sort-in-javascript/
+  // while ((j > -1) && (current < inputArr[j])) {
+  //   inputArr[j+1] = inputArr[j];
+  //   j--;
+  // }
 }
 
 export { insertionSort };
