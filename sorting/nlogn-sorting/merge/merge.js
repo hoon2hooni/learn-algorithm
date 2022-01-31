db7bbd4ace961660cd0dd678c44a5bf2aa8b5a30
hoon2hooni <sanghoon.engineer@gmail.com>
2022-01-31 18:58:24 +0900

    refac: refactor redundant logic in merge funciton

--- sorting/nlogn-sorting/merge/merge.js
+++ sorting/nlogn-sorting/merge/merge.js
@@ -3,7 +3,6 @@ function merge(sortedL, sortedR) {
   let [leftLength, rightLength] = [sortedL.length, sortedR.length];
   let [i, j] = [0, 0];
   const answer = [];
-  let temp;
   while (i < leftLength && j < rightLength) {
     if (sortedL[i] < sortedR[j]) {
       answer.push(sortedL[i]);
@@ -12,21 +11,17 @@ function merge(sortedL, sortedR) {
       answer.push(sortedR[j]);
       j++;
     }
-    if (i === leftLength) temp = "l";
-    if (j === rightLength) temp = "r";
   }
 
-  if (temp === "l") {
-    while (j < rightLength) {
-      answer.push(sortedR[j]);
-      j++;
-    }
-  } else {
-    while (i < leftLength) {
-      answer.push(sortedL[i]);
-      i++;
-    }
+  while (j < rightLength) {
+    answer.push(sortedR[j]);
+    j++;
   }
+  while (i < leftLength) {
+    answer.push(sortedL[i]);
+    i++;
+  }
+
   return answer;
 }
 
